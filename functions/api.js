cad2707a09bef41e3496c22bca582bbb5c4ff811
EndzyCodes <markgilbertreginio96204@gmail.com>
2024-04-21 09:11:06 +0800

    fix netlify.toml

--- functions/api.js
+++ functions/api.js
@@ -5,22 +5,23 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-// const dbCloudUrl = "mongodb+srv://mark1:zci5KHjd7wUsqcwe@cluster0.fksli2l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dbCloudUrl = "mongodb+srv://mark1:zci5KHjd7wUsqcwe@cluster0.fksli2l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const dbLocalUrl = "mongodb://localhost:27017/serverless-api";
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// app.get('/.netlify/functions/', (req, res) => {
-//     res.json({message: 'Hello World'});
-// });
-
 //Connect to mongodb
 mongoose
-    .connect(dbLocalUrl)
+    .connect(dbCloudUrl || dbLocalUrl)
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error("Failed to connect to MongoDB", error));
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
 
 app.use('/.netlify/functions/api', router);
 
